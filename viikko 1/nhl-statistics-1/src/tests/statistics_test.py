fb1d025a7a9a9ea5e3ed722b29f0d8e4ade35af6
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-11-01 16:31:56 +0200

    Tehtävä 16

---
+++ viikko 1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,36 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search(self):
+        player = str(self.stats.search('Semenko'))
+        self.assertEqual(player, str(Player("Semenko", "EDM", 4, 12)))
+
+    def test_search_not_existing_player(self):
+        player = self.stats.search('Testi')
+        self.assertEqual(player, None)
+
+    def test_team(self):
+        players = self.stats.team('PIT')
+        self.assertEqual(list(map(str, players)), [str(Player("Lemieux", "PIT", 45, 54))])
+
+    def test_top(self):
+        top = self.stats.top(2)
+        self.assertEqual(list(map(str, top)), [str(Player("Gretzky", "EDM", 35, 89)), str(Player("Lemieux", "PIT", 45, 54)), str(Player("Yzerman", "DET", 42, 56))])
